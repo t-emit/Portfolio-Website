b2146637b8c05db2b3d39cba8ebff7a925a2e6d4
t-emit <t.siddiqui2814@gmail.com>
2025-07-28 13:47:03 +0530

    Initial commit: Setup frontend and backend structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import MainApp from './components/MainApp'; // rename your App content here
 
@@ -6,13 +7,15 @@ function App() {
   const [entered, setEntered] = useState(false);
 
   return (
-    <>
+    
+    <BrowserRouter basename="/Portfolio-Website">
       {!entered ? (
         <LandingPage onEnter={() => setEntered(true)} />
       ) : (
         <MainApp />
       )}
-    </>
+      </BrowserRouter>
+    
   );
 }
 
