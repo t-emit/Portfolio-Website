bf31cbe1f6d6a9941a463be25f7149461dc352a2
t-emit <t.siddiqui2814@gmail.com>
2025-07-28 13:47:28 +0530

    Initial commit: Setup frontend and backend structure

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // Your MainApp.jsx
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+// import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* Wrap your App component */}
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
